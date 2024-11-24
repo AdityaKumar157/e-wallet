82faebcd2cd9d091b2690494cdbd270fdf566afd
AdityaKumar157 <49588338+AdityaKumar157@users.noreply.github.com>
2024-11-24 16:08:58 +0530

    <Feature> Implemented UserService in UserServiceImpl class.

--- user-accounts/src/main/java/com/makeprojects/ewallet/useraccounts/api/controller/UserController.java
+++ user-accounts/src/main/java/com/makeprojects/ewallet/useraccounts/api/controller/UserController.java
@@ -2,7 +2,7 @@ package com.makeprojects.ewallet.useraccounts.api.controller;
 
 import com.makeprojects.ewallet.shared.exceptions.NotFoundException;
 import com.makeprojects.ewallet.shared.database.model.User;
-import com.makeprojects.ewallet.useraccounts.core.service.implementation.UserServiceImpl;
+import com.makeprojects.ewallet.useraccounts.core.service.definition.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,17 +15,17 @@ import java.util.UUID;
 @RequestMapping("/user")
 public class UserController {
 
-    private final UserServiceImpl userService;
+    private final UserService userService;
 
     @Autowired
-    public UserController(UserServiceImpl userService) {
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
     @PostMapping("/add")
     public ResponseEntity<User> addUser(@RequestBody User user) {
         try {
-            User addedUser = this.userService.addUser(user);
+            User addedUser = this.userService.create(user);
             return new ResponseEntity<>(addedUser, HttpStatus.CREATED);
         } catch (Exception e) {
             return ResponseEntity.internalServerError().build();
@@ -35,7 +35,7 @@ public class UserController {
     @GetMapping("/{userId}")
     public ResponseEntity<User> getUserById(@PathVariable UUID userId) {
         try {
-            User user = this.userService.getUserById(userId);
+            User user = this.userService.get(userId);
             return ResponseEntity.ok(user);
         } catch (NotFoundException e) {
             return ResponseEntity.notFound().build();
--- user-accounts/src/main/java/com/makeprojects/ewallet/useraccounts/core/service/implementation/UserServiceImpl.java
+++ user-accounts/src/main/java/com/makeprojects/ewallet/useraccounts/core/service/implementation/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.makeprojects.ewallet.shared.exceptions.NotFoundException;
 import com.makeprojects.ewallet.shared.database.model.Account;
 import com.makeprojects.ewallet.shared.database.model.User;
 import com.makeprojects.ewallet.useraccounts.core.service.definition.AccountService;
+import com.makeprojects.ewallet.useraccounts.core.service.definition.UserService;
 import com.makeprojects.ewallet.useraccounts.database.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,16 +12,19 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
 @Slf4j
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final AccountService accountService;
     private final PasswordEncoder passwordEncoder;
+
+    private static final String EMPTY_STRING = "";
 
     @Autowired
     public UserServiceImpl(UserRepository userRepository, AccountService accountService, PasswordEncoder passwordEncoder) {
@@ -29,38 +33,137 @@ public class UserServiceImpl {
         this.passwordEncoder = passwordEncoder;
     }
 
-    @Transactional(rollbackFor = Exception.class)
-    public User addUser(User user) {
+    //<editor-fold desc="UserService Implementation">
+    @Override
+    public User get(UUID id) {
+        String errorMsg = EMPTY_STRING;
         try {
-            User savedUser = this.userRepository.save(user);
-            savedUser.setRole("USER");
-            savedUser.setPassword(this.passwordEncoder.encode(savedUser.getPassword()));
-            Account savedUserAccount = this.accountService.addAccount(savedUser);
-            log.info(String.format("Successfully saved user with userId %s, accountId %s.", savedUser.getUserId(), savedUserAccount.getAccountId()));
-            return savedUser;
-        } catch (Exception e) {
-            String error = String.format("Exception while saving user with userID %s. Exception: %s", user.getUserId(), e);
-            log.error(error);
-            throw e;
-        }
-    }
-
-    public User getUserById(UUID userId) {
-        try {
-            Optional<User> optionalUser = this.userRepository.findById(userId);
-            if(optionalUser.isEmpty()) {
-                NotFoundException ex = new NotFoundException(User.class, "userId", userId);
-                log.error(ex.getMessage());
-                throw ex;
+            if (id == null) {
+                errorMsg = String.format("UUID of User cannot be null.");
+                log.error(errorMsg);
+                throw new NullPointerException(errorMsg);
             }
 
-            User user = optionalUser.get();
-            log.info("Successfully fetched user with userId {}.", user.getUserId());
-            return user;
+            Optional<User> optionalUser = this.userRepository.findById(id);
+            if (optionalUser.isEmpty()) {
+                errorMsg = String.format("User with UUID %s is not found.", id);
+                log.error(errorMsg);
+                throw new NotFoundException(User.class, "UUID", id);
+            }
+
+            log.info(String.format("Successfully retrieved User with UUID '%s'.", id));
+            return optionalUser.get();
         } catch (Exception e) {
-            String error = String.format("Exception while fetching user with userID %s. Exception: %s", userId, e);
-            log.error(error);
+            errorMsg = String.format("Exception occurred while retrieving User with UUID '%s'. Exception: %s", id, e);
+            log.error(errorMsg);
             throw e;
         }
     }
+
+    @Override
+    public List<User> getAll() {
+        try {
+            List<User> userList = this.userRepository.findAll();
+            if (userList.isEmpty()) {
+                log.error("No User found.");
+            }
+
+            log.info("Successfully retrieved Users list.");
+            return userList;
+        } catch (Exception e) {
+            log.error("Exception occurred while retrieving list of Users.");
+            throw e;
+        }
+    }
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public User create(User entity) {
+        String errorMsg = EMPTY_STRING;
+        try {
+            if (entity == null) {
+                log.error("User entity cannot be null.");
+                throw new NullPointerException("User entity cannot be null.");
+            }
+
+            if ((entity.getUserId() != null) && (get(entity.getUserId()) != null)) {
+                errorMsg = String.format("User with UUID %s already exists.", entity.getUserId());
+                log.error(errorMsg);
+                throw new RuntimeException(errorMsg);
+            }
+
+            entity.setRole("USER");
+            entity.setPassword(this.passwordEncoder.encode(entity.getPassword()));
+            User createdUser = this.userRepository.save(entity);
+            if (createdUser == null) {
+                log.error("Failed to create an user.");
+                throw new RuntimeException("Failed to create an user.");
+            }
+
+            Account savedUserAccount = this.accountService.addAccount(createdUser);
+            log.info(String.format("Successfully saved user with userId %s, accountId %s.", createdUser.getUserId(), savedUserAccount.getAccountId()));
+
+            return createdUser;
+        } catch (Exception e) {
+            errorMsg = String.format("Exception occurred while creating an user. Exception: %s", e);
+            log.error(errorMsg);
+            throw e;
+        }
+    }
+
+    @Override
+    public User update(User entity) {
+        String errorMsg = EMPTY_STRING;
+        try {
+            if (entity == null) {
+                log.error("User entity which needs to be updated cannot be null.");
+                throw new NullPointerException("User entity cannot be null.");
+            }
+
+            if (get(entity.getUserId()) == null) {
+                errorMsg = String.format("User with UUID %s doesn't exist in database.", entity.getUserId());
+                log.error(errorMsg);
+                throw new NotFoundException(User.class, "UUID", entity.getUserId());
+            }
+
+            User updatedUser = this.userRepository.save(entity);
+            if (updatedUser == null) {
+                errorMsg = String.format("Failed to update an User with UUID '%s'.", entity.getUserId());
+                log.error(errorMsg);
+                throw new RuntimeException(errorMsg);
+            }
+
+            log.info(String.format("Successfully updated an user with UUID '%s'.", updatedUser.getUserId()));
+            return updatedUser;
+        } catch (Exception e) {
+            errorMsg = String.format("Exception occurred while creating an user. Exception: %s", e);
+            log.error(errorMsg);
+            throw e;
+        }
+    }
+
+    @Override
+    public void delete(UUID id) {
+        String errorMsg = EMPTY_STRING;
+        try {
+            if (id == null) {
+                log.error("User entity UUID which needs to be deleted cannot be null.");
+                throw new NullPointerException("User entity UUID which needs to be deleted cannot be null.");
+            }
+
+            if (get(id) == null) {
+                errorMsg = String.format("User with UUID %s doesn't exist in database.", id);
+                log.error(errorMsg);
+                throw new NotFoundException(User.class, "UUID", id);
+            }
+
+            this.userRepository.deleteById(id);
+            log.info(String.format("Successfully deleted an user with UUID '%s'.", id));
+        } catch (Exception e) {
+            errorMsg = String.format("Exception occurred while deleting an user with UUID '%s'. Exception: %s", id, e);
+            log.error(errorMsg);
+            throw e;
+        }
+    }
+    //</editor-fold>
 }
